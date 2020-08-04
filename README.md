995bc2b22933c3b38211d0d80116d81868bb9a2a
ben <57264435+ovsiew@users.noreply.github.com>
2020-08-04 16:16:22 -0400

    Fixed post scrolling
    other minor formatting tweaks

--- README.md
+++ README.md
@@ -1,38 +1,11 @@
 # nebula
 Hexo theme  (still in development)
 
-## Build
+## build
 ```
-$ git clone https://github.com/bendotbike/nebula-theme.git $working_directory$
+$ git clone https://github.com/bendotbike/nebula.git $working_directory$
 $ cd %working_directory%
 $ npm install
-$ grunt build #http://localhost:4000
+$ hexo generate
+$ hexo serve #http://localhost:4000
 ```
-
-## Config
-### _config.yml
-
-### styles.scss
-
-## Grunt
-This theme makes use of a few grunt scripts to make site development easier
-- Build (```grunt build```): generates site and serves it on a local server
-- Production (```grunt production```): generates site, minifies html, css, and js. Drag ```public/``` to your production server and your site is ready
-
-## New pages
-- Post: ```hexo new PostTitle```
-- Page: ```hexo new page PageTitle```
-- Download page: ```hexo new page --path path/to/downloads/my-download "Title"```. Add ```layout: download``` to front-matter
-- Project page: ```hexo new page --path path/to/projects/my-project "Title"```. Add ```layout: project``` to front-matter
-- Project index page (Displays all 'project' pages): ```hexo new page --path path/to/projects/ "Title"```. Add ```layout: project_index``` to front-matter
-
-## To-Do
-- Finish README documentation
-- README formatting
-- Built-in landing-page on homepage
-- Combine/refactor CSS for articles and project pages
-- Page meta tags
-- Tweak project pages
-- Put page titles in navbar
-- Post jump margin fix
-- Post jump margin animation
--- themes/nebula/source/js/site.js
+++ themes/nebula/source/js/site.js
@@ -9,6 +9,7 @@ var navOpen;
 
 window.onload = function() {
 
+    // Define variables
     navOpen = false;
 
     // ** NOTE **
@@ -18,41 +19,55 @@ window.onload = function() {
     // Grab all highlighted code and put "copy to clipboard" buttons above
     highlightedBlocks = document.getElementsByClassName("highlight");
     for (i = 0; i < highlightedBlocks.length; i++) {
+        var codeText = highlightedBlocks[i].querySelector("td.code").innerText;
         var copyBtn = "<a class='copy-code-btn' href='javascript:copyCode(" + i + ");'>Copy to clipboard</a>";
-        highlightedBlocks[i].innerHTML += "<p>" + copyBtn + "</p><br>"; // Inserts button at bottom of snippet 
+        highlightedBlocks[i].innerHTML += "<p>" + copyBtn + "</p><br>"; // Inserts button at bottom of snippet
     }
 
     // If we are on a 'post' (indicated by sidebar), then insert post headers into the sidebar as quick-links
     if (document.getElementById("post-sidebar")) {
         var headers = document.querySelectorAll(".post-content h1, .post-content h2, .post-content h3, .post-content h4, .post-content h5, .post-content h6");
-
+        document.getElementById("post-sidebar--post-headers").innerHTML += "<ul>";
         for (i = 0; i < headers.length; i++) {
-            text = headers[i].textContent;
-
-            // Fix up url (lol) @todo there is a better way to do this
-            var url = "#" + text.replace(/\s+/g, "-"); // replace spaces w/ dashes
+            var text = headers[i].textContent;
+            // do some basic regex to get the #anchor url, add '#', remove spaces and replace them with dashes, and remove () and other symbols
+            var url = "#" + text.replace(/\s+/g, "-");
             url = url.replace(",", "");
             url = url.replace("(", "");
             url = url.replace(")", "");
-            url = url.replace("!", "");
             url = url.replace("?", "");
-            url = url.replace(".", "");
-
-            document.getElementById("post-sidebar--post-headers").innerHTML += "<li><a href='javascript:jumpTo(\"" + url + "\");'>" + text + "</a></li>";
+            document.getElementById("post-sidebar--post-headers").innerHTML += "<li><a href='javascript:jumpTo(\"" + url + "\")'>" + text + "</a></li>";
         }
-        
     }
 
+}
+
+// Jumps to a sidebar location. Accounts for fixed navbar
+function jumpTo(jumpHash) {
+    // Jump to the sidebar location offset by 40 pixels upwards, to account for the fixed navbar
+    var el = document.getElementById(jumpHash.split("#")[1]);
+    var scrollPos = el.offsetTop - 40;
+    window.scrollTo(0, scrollPos);
+    // Set hash (with fallback) to reflect the page jump
+    if (history.pushState) {
+        history.pushState(null, null, jumpHash);
+    } else {
+        location.hash = jumpHash;
+    }
 }
 
 // Copies code to clipboard
 // Creates a hidden text area to copy from
 // i = index of .highlight block (this method is called from onLoad function above)
 function copyCode(i) {
-    copy(highlightedBlocks[i].querySelector("td.code").innerText);
-
+    var copyTextArea = document.createElement("textarea");
+    document.body.appendChild(copyTextArea);
+    copyTextArea.value = highlightedBlocks[i].querySelector("td.code").innerText;
+    copyTextArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(copyTextArea);
     highlightedBlocks[i].querySelector("a.copy-code-btn").innerText = "Copied!";
-    setTimeout(() => {
+    setTimeout(function() {
         highlightedBlocks[i].querySelector("a.copy-code-btn").innerText = "Copy to clipboard";
     }, 3000);
 }
@@ -66,32 +81,4 @@ function toggleNav() {
         document.getElementById("navbar--content").style.width = "300px";
         navOpen = true;
     }
-}
-
-// For jumping to #locations on posts
-// Jumps to location & does animation
-function jumpTo(location) {
-    var newLocation = location;
-    if (location.split("")[0] != "#") {
-        newLocation = "#" + location;
-    }
-    document.location.hash = newLocation;
-
-    //document.getElementById(newLocation.split("#")[1]).style.color = "red";
-    document.getElementById(newLocation.split("#")[1]).classList.add("fade-in");
-
-}
-
-/*
-*   Utilities
-*/
-
-// Copies text to clipboard
-function copy(text) {
-    var copyTextArea = document.createElement("textarea");
-    document.body.appendChild(copyTextArea);
-    copyTextArea.value = text;
-    copyTextArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(copyTextArea);
 }
